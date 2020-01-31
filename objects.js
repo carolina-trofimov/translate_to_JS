b56faa6bcaa02243b96bb5f9222e099db8038a1e
Your Name <you@example.com>
2020-01-30 17:47:54 -0800

    Completed translating arrays, functions, and objects to JS.

---
+++ objects.js
@@ -0,0 +1,33 @@
+"use strict";
+
+
+// 1. countWords
+function countWords(phrase) {
+  const wordCounts = {}
+
+  for (const word of phrase.split(' ')) {
+    if (wordCounts.hasOwnProperty(word)) {
+      wordCounts[word] += 1;
+    } else {
+      wordCounts[word] = 1;
+    }
+  } return wordCounts;
+}
+
+
+// 2. getMelonsAtPrice
+function getMelonsAtPrice(price) {
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+ }
+
+  if (!melonPrices.hasOwnProperty(price)) {
+    return; 
+}
+
+  return melonPrices[price].sort();
+
+}
