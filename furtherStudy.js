bef8691a641a78f1c256de34d48a548e240fbd44
Your Name <you@example.com>
2020-01-31 13:03:04 -0800

    further study unfinished

--- furtherStudy.js
+++ furtherStudy.js
@@ -2,10 +2,44 @@
 
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const words1Set = new Set(words1);
+  const words2Set = new Set(words2);
+
+  const result = new Set();
+
+  for (const word of words1Set){
+    if (words2Set.has(word)) {
+      result.add(word);
+    }
+  }
+  return Array.from(result);
 }
 
 
 function kidsGame(names) {
-  // Replace this with your code
+  const output = [names.shift()];
+  console.log(output);
+
+  const firstLetterToWords = {};
+
+  for (const name of names) {
+    if (!firstLetterToWords[name[0]]) {
+      firstLetterToWords[name[0]] = [name];
+    } else {
+      firstLetterToWords[name[0]].push(name);
+    }
+  }
+
+  while (true) {
+    const startWord = output[output.length -1];
+    const startLetter = startWord[startWord.length -1];
+
+    if (!firstLetterToWords[startLetter]) {
+      break;
+    }
+
+    const word = firstLetterToWords[startLetter].shift();
+    output.push(word);
+  }
+  return output;
 }
